2954aac6a87e48b1e3e17920922c4062d48ce563
up637415 <up637415@myport.ac.uk>
2015-10-11 20:54:48 +0100

    Testing jQuery click function.

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,8 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
     console.log( "ready!" );
-    $(".well").html("<p>test jQuery</p>");
+    // $(".well").html("<p>test jQuery</p>");
+    $("p").click(function(){
+    alert("The paragraph was clicked.");
+});
 });
