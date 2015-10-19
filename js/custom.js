abc49090502a6580d278e03cfe37d738219380b2
up637415 <up637415@myport.ac.uk>
2015-10-19 21:47:22 +0100

    Updated 0870 ajax function.

--- js/custom.js
+++ js/custom.js
@@ -9,36 +9,47 @@ $( document ).ready(function() {
     $('#results').hide();
 
     // get value of telephone number input box on key up.
-    $('#phoneNoS').keypress(function() {
+    $('#phoneNo').keyup(function() {
       var dInput = this.value;
-      // if (dInput.length > 4) {
-      //   console.log("freephone");
-      // }
-      // console.log(dInput);
 
-      // Example for natwest http://up637415.co.uk/node.json?field_free_phone=0800 200400
+      // Check if 0870 number.
+      if (dInput.indexOf("0870") >= 0) {
 
-      // jQuery JSON.
-      var freeAlternativeAPI = "http://up637415.co.uk/node.json?field_premium_number=" + dInput;
-      var data_fields = $.getJSON( freeAlternativeAPI, function(data) {
+        $('#results').empty();
+        console.log("0870 detected.");
 
-        // Get all of the fields.
-        var list_of_fields = data.list[0];
-        console.log(list_of_fields); 
+        // Check if length is greater than 4
+        if (dInput.length >= 12 && dInput.length <= 13) {
 
-        // Get value of title field.
-        var node_title = list_of_fields.title;
-        if (node_title.length){
-          $('#results').show();
-          $( "<h2>" + node_title + "</h2>" ).appendTo( "#results" );
-          console.log("title exists");
+          // jQuery JSON.
+          var freeAlternativeAPI = "http://up637415.co.uk/node.json?field_premium_number=" + dInput;
+          var data_fields = $.getJSON( freeAlternativeAPI, function(data) {
+
+            // Get all of the fields.
+            var list_of_fields = data.list[0];
+            console.log(list_of_fields);
+
+            // Get value of title field.
+            var node_title = list_of_fields.title;
+            if (node_title.length){
+              $('#results').show();
+              $( "<p>" + node_title + "</>" ).appendTo( "#results" );
+              console.log("title exists");
+            }
+            var free_phone_numbers = list_of_fields.field_free_phone;
+            $.each( free_phone_numbers, function( key, value ) {
+              $( "<p>" + value + "</p>" ).appendTo( "#results" );
+              //console.log(formatted_free_numbers);
+            });
+          });
+        }else {
+          $('#results').hide();
         }
-        var free_phone_numbers = list_of_fields.field_free_phone;
-        $.each( free_phone_numbers, function( key, value ) {
-          $( "<p>" + value + "</p>" ).appendTo( "#results" );
-          //console.log(formatted_free_numbers);
-        });
-      });
+        console.log(dInput);
+
+
+      }
+
     });
 
 });
