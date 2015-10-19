a45405b88987bc0440efde3cee508d7e585d0c6b
up637415 <up637415@myport.ac.uk>
2015-10-19 21:11:20 +0100

    Basic functionality of number search completed. Needs a lot more work as it is very buggy.

--- js/custom.js
+++ js/custom.js
@@ -5,43 +5,40 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
+    // On load, hide result div (not needed).
+    $('#results').hide();
+
     // get value of telephone number input box on key up.
     $('#phoneNoS').keypress(function() {
       var dInput = this.value;
-      if (dInput.length > 4) {
-        console.log("freephone");
-      }
-      console.log(dInput);
+      // if (dInput.length > 4) {
+      //   console.log("freephone");
+      // }
+      // console.log(dInput);
 
       // Example for natwest http://up637415.co.uk/node.json?field_free_phone=0800 200400
 
       // jQuery JSON.
-      var freeAlternativeAPI = "http://up637415.co.uk/node.json?field_free_phone=" + dInput;
-      $.getJSON( freeAlternativeAPI, function( data ) {
-        dataString = JSON.stringify(data);
-        console.log("Data:::::" + dataString);
-        var items = JSON.parse(dataString);
+      var freeAlternativeAPI = "http://up637415.co.uk/node.json?field_premium_number=" + dInput;
+      var data_fields = $.getJSON( freeAlternativeAPI, function(data) {
 
-        $(items).each(function(){
+        // Get all of the fields.
+        var list_of_fields = data.list[0];
+        console.log(list_of_fields); 
 
+        // Get value of title field.
+        var node_title = list_of_fields.title;
+        if (node_title.length){
+          $('#results').show();
+          $( "<h2>" + node_title + "</h2>" ).appendTo( "#results" );
+          console.log("title exists");
+        }
+        var free_phone_numbers = list_of_fields.field_free_phone;
+        $.each( free_phone_numbers, function( key, value ) {
+          $( "<p>" + value + "</p>" ).appendTo( "#results" );
+          //console.log(formatted_free_numbers);
         });
-
-        // $.each( data, function( key, val ) {
-        //   console.log("Val:::::" + val);
-        //   console.log("Key:::::" + key);
-        // });
-        // $( "<ul/>", {
-        //   "class": "my-new-list",
-        //   html: items.join( "" )
-        // }).appendTo( "#response-text" );
       });
-    });
-
-    $( document ).on( "vclick", ".SeeMore2", function() {
-    // $(this).hide();
-
-
-
     });
 
 });
